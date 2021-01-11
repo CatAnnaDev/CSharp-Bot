a2e9f1d6b6670e1b3e033d7221591551d6728140
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2021-01-11 13:49:00 +0100

    Add files via upload

--- Modules/Help.cs
+++ Modules/Help.cs
@@ -2,8 +2,7 @@
 using Discord;
 using Discord.Commands;
 using System;
-using Discord.WebSocket;
-using System.Configuration;
+
 
 namespace csharp_discord_bot.Modules
 {
@@ -13,21 +12,12 @@ namespace csharp_discord_bot.Modules
         public async Task HelpAsync()
         {
             var builder = new EmbedBuilder()
-    .WithTitle("~(c# bot help )~")
+    .WithTitle("~(C# bot help )~")
     .WithColor(new Color(0xFF00FF))
     .WithTimestamp(DateTimeOffset.FromUnixTimeMilliseconds(1610151618624))
-    .AddField("!ping", "ping le bot")
-    .AddField("!love", "Love pic")
-    .AddField("!gif", "Kittens gif")
-    .AddField("!avatar", "retrouve le lien de vôtre avatar discord")
-    .AddField("!react", "auto react a une phrase")
-    .AddField("!meow", "Kittens gif / pic")
-    .AddField("!dog", "Dogs gif / pic")
-    .AddField("!meme", "Meme gif / pic")
-    .AddField("!say", "repeat your last sentence")
-    .AddField("!square", "square a number")
-    .AddField("!userinfo", "return info about the current user")
-    .AddField("!asian", "fun fun fun");
+    .AddField("Prefix:", ":exclamation:")
+    .AddField("Command:", "[Click Here](https://github.com/PsykoDev/CSharp-Bot/blob/main/Doc/Command.md)")
+    .AddField("Need Help ?", "[Click Here](https://github.com/PsykoDev/CSharp-Bot/issues)");
             var embed = builder.Build();
             await ReplyAsync(
                 null,
@@ -35,32 +25,5 @@ namespace csharp_discord_bot.Modules
                 .ConfigureAwait(false);
         }
 
-        [Command("TEST")]
-        public async Task TestAsync(SocketUser user = null)
-        {
-                var userInfo = user ?? Context.Client.CurrentUser;
-                await ReplyAsync(
-                    $"{userInfo.Username}#{userInfo.Discriminator} \n" +
-                    $"is: {userInfo.Status}");
-        }
-
-        [Command("print")]
-        public async Task PrintAsync()
-        {
-            config();
-        }
-
-        public void config()
-        {
-            Console.WriteLine("Count: " + ConfigurationManager.AppSettings.Count);
-            Console.WriteLine("Token: " + ConfigurationManager.AppSettings.Get("Token"));
-            Console.WriteLine("Cats: " + ConfigurationManager.AppSettings.Get("Cats"));
-            Console.WriteLine("Dogs: " + ConfigurationManager.AppSettings.Get("Dogs"));
-            Console.WriteLine("giphelove: " + ConfigurationManager.AppSettings.Get("giphelove"));
-            Console.WriteLine("giphykittens: " + ConfigurationManager.AppSettings.Get("giphykittens"));
-            Console.WriteLine("meme: " + ConfigurationManager.AppSettings.Get("meme"));
-            Console.WriteLine("porn: " + ConfigurationManager.AppSettings.Get("porn"));
-            Console.WriteLine("Hellow: " + ConfigurationManager.AppSettings.Get("Hellow"));
-        }
     }
 }
