05ac47398d795382140068f306353eb257502523
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2021-01-12 04:23:41 +0100

    Add files via upload

--- Modules/Help.cs
+++ Modules/Help.cs
@@ -13,10 +13,11 @@ namespace csharp_discord_bot.Modules
             var builder = new EmbedBuilder()
     .WithTitle("~(C# bot help )~")
     .WithColor(new Color(0xFF00FF))
-    .WithTimestamp(DateTimeOffset.FromUnixTimeMilliseconds(1610151618624))
-    .AddField("Prefix:", ":exclamation:")
-    .AddField("Command:", "[Click Here](https://github.com/PsykoDev/CSharp-Bot/blob/main/Doc/Command.md)")
-    .AddField("Need Help ?", "[Click Here](https://github.com/PsykoDev/CSharp-Bot/issues)");
+                    .WithDescription(
+                    $"** Prefix: ** :exclamation:\n" +
+                    $"** Command: ** [Click Here](https://github.com/PsykoDev/CSharp-Bot/blob/main/Doc/Command.md)\n" +
+                    $"** Need Help: ** [Click Here](https://github.com/PsykoDev/CSharp-Bot/issues)\n")
+    .WithTimestamp(DateTimeOffset.FromUnixTimeMilliseconds(1610151618624));
             var embed = builder.Build();
             await ReplyAsync(
                 null,
