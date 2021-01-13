b3310d0e3e5a7c1991e253cfe1dacea14019e7e1
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2021-01-13 10:20:28 +0100

    Add files via upload

---
+++ Modules/Mod.cs
@@ -0,0 +1,60 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace csharp_discord_bot
+{
+    public class Mod : ModuleBase<SocketCommandContext>
+    {
+        [Command("ban")]
+        [Summary("Ban a user from the server")]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [RequireBotPermission(GuildPermission.BanMembers)]
+        public async Task Ban(SocketGuildUser targetUser, [Remainder] string reason = "No reason provided.")
+        {
+            await Context.Guild.AddBanAsync(targetUser.Id, 0, reason);
+            await ReplyAsync($"**{targetUser}** has been banned. Bye bye :wave:");
+        }
+
+        [Command("kick")]
+        [Summary("Kick a user from the server.")]
+        [RequireBotPermission(GuildPermission.KickMembers)]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        public async Task Kick(SocketGuildUser targetUser, [Remainder] string reason = "No reason provided.")
+        {
+            await targetUser.KickAsync(reason);
+            await ReplyAsync($"**{targetUser}** has been kicked. Bye bye :wave:");
+        }
+
+        [Command("purge")]
+        [Summary("Bulk deletes messages in chat")]
+        [RequireBotPermission(GuildPermission.ManageMessages)]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        public async Task Purge(int delNumber)
+        {
+            var channel = Context.Channel as SocketTextChannel;
+            var items = await channel.GetMessagesAsync(delNumber + 1).FlattenAsync();
+            await channel.DeleteMessagesAsync(items);
+        }
+
+        [Command("reloadconfig")]
+        [Summary("Reloads the config and applies changes")]
+        [RequireOwner]
+        public async Task ReloadConfig()
+        {
+            await Functions.SetBotStatusAsync(Context.Client);
+            await ReplyAsync("Reloaded!");
+        }
+
+        [Command("unban")]
+        [Summary("Unban a user from the server")]
+        [RequireBotPermission(GuildPermission.BanMembers)]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        public async Task Unban(ulong targetUser)
+        {
+            await Context.Guild.RemoveBanAsync(targetUser);
+            await Context.Channel.SendMessageAsync($"The user has been unbanned :clap:");
+        }
+    }
+}
